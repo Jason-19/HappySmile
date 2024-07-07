19e0202762e0cfa6a3388e300416a72cc0c6e7bd
Jason-19 <jasonnunez02016@gmail.com>
2024-07-07 15:01:56 -0500

    Primer Commit

---
+++ CONFIG/connection.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "proyecto";
+
+// Crear una nueva conexión con la base de datos 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+     echo "SQL❌";
+     die("Conexión fallida: " . $conn->connect_error);
+} else {
+     echo "SQL✅";
+}
---
+++ Php/header.php
@@ -0,0 +1,5 @@
+<div class="links">
+     <a href="AEROLINEA.php">AEROLINEA</a>
+     <a href="Clinica.php">CLÍNICA</a>
+     <a href="index.php">HOSPITAL</a>
+</div>
