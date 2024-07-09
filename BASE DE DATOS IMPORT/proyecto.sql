5f86c141751ed4bea89dbe57230aa234b054d8ce
Jason Núñez <101609449+Jason-19@users.noreply.github.com>
2024-07-09 18:43:28 -0500

    Add files via upload
    add db

--- BASE DE DATOS IMPORT/proyecto.sql
+++ BASE DE DATOS IMPORT/proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-07-2024 a las 20:11:32
+-- Tiempo de generación: 10-07-2024 a las 01:42:33
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -24,11 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `consult`
+--
+
+CREATE TABLE `consult` (
+  `CONSULT_ID` int(11) NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `DOCTOR_ID` int(11) DEFAULT NULL,
+  `TRATAMIENTO` varchar(50) DEFAULT NULL,
+  `FECHA_CONSULT` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `DOCTOR_ID` int(11) NOT NULL,
+  `NOMBRE` varchar(50) DEFAULT NULL,
+  `APELLIDO` varchar(50) DEFAULT NULL,
+  `ESPECIALIDAD` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `FACTURAS_ID` int(11) NOT NULL,
+  `CONSULT_ID` int(11) DEFAULT NULL,
+  `FECHA_FACTURA` date DEFAULT NULL,
+  `MONTO` decimal(10,0) DEFAULT NULL,
+  `METODO_PAGO` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
-  `ID` int(11) NOT NULL,
+  `USUARIO_ID` int(11) NOT NULL,
   `NOMBRE` varchar(50) NOT NULL,
   `APELLIDO` varchar(50) NOT NULL,
   `CEDULA` varchar(50) NOT NULL,
@@ -42,31 +83,84 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`ID`, `NOMBRE`, `APELLIDO`, `CEDULA`, `EMAIL`, `PHONE`, `TRATAMIENTO`, `FECHA_FACTURA`) VALUES
-(1, 'Karla', 'Aranda', '4-888-1457', 'Karla02@gmail.com', '6554-4532', 'Limpieza dental', '2024-07-09 17:18:53'),
-(2, 'Marta', 'Caballero', '4-888-1457', 'marta123@gmail.com', '6543-9801', '', '2024-07-09 17:57:24'),
-(3, 'Marta', 'Caballero', '4-888-1457', 'marta123@gmail.com', '6543-9801', '', '2024-07-09 17:58:20'),
-(4, 'Marta', 'Caballero', '4-888-1457', 'marta123@gmail.com', '6543-9801', '', '2024-07-09 17:59:00');
+INSERT INTO `usuarios` (`USUARIO_ID`, `NOMBRE`, `APELLIDO`, `CEDULA`, `EMAIL`, `PHONE`, `TRATAMIENTO`, `FECHA_FACTURA`) VALUES
+(8, 'Karla', 'Aranda', '4-888-145', 'Karla02@gmail.com', '6543-9801', 'Limpieza dental', '2024-07-09 21:01:27');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `consult`
+--
+ALTER TABLE `consult`
+  ADD PRIMARY KEY (`CONSULT_ID`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `DOCTOR_ID` (`DOCTOR_ID`);
+
+--
+-- Indices de la tabla `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`DOCTOR_ID`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`FACTURAS_ID`),
+  ADD KEY `CONSULT_ID` (`CONSULT_ID`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`USUARIO_ID`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `consult`
+--
+ALTER TABLE `consult`
+  MODIFY `CONSULT_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `DOCTOR_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `FACTURAS_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `USUARIO_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `consult`
+--
+ALTER TABLE `consult`
+  ADD CONSTRAINT `consult_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`USUARIO_ID`),
+  ADD CONSTRAINT `consult_ibfk_2` FOREIGN KEY (`DOCTOR_ID`) REFERENCES `doctor` (`DOCTOR_ID`);
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`CONSULT_ID`) REFERENCES `consult` (`CONSULT_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
