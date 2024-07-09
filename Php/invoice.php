03d62f1f36dc57e011c7a19d051f79a6e6b0d3a3
Jason-19 <jasonnunez02016@gmail.com>
2024-07-09 18:40:43 -0500

    user

--- Php/invoice.php
+++ Php/invoice.php
@@ -16,33 +16,73 @@ $result = $conn->query($sql);
 <html lang="en">
 
 <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Invoice</title>
-     <link rel="stylesheet" href="../CSS/css.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invoice</title>
+    <link rel="stylesheet" href="../CSS/css.css">
 
 </head>
 
 <body class="bodyInvoise">
-     <div class="links">
-          <a href="index.php">Home</a>
+    <div class="links">
+        <a href="index.php">Home</a>
 
-     </div>
-     <div class="navbar">
-          <div class="image">
-               <a href="index.php">
-                    <img src="../image/logo.png" alt="imagen">
-               </a>
-          </div>
-          <div class="titleVuelo">
-               <h1>HAPPY SMILES´S</h1>
-          </div>
+    </div>
+    <div class="navbar">
+        <div class="image">
+            <a href="index.php">
+                <img src="../image/logo.png" alt="imagen">
+            </a>
+        </div>
+        <div class="titleVuelo">
+            <h1>HAPPY SMILES´S</h1>
+        </div>
 
-     </div>
-     <div class="container_invoise">
-          <h1>Clientes y Consultas</h1>
-          <table>
-               <tr>
+    </div>
+    <div class="container_invoise">
+    <div class="userInfo">
+            <h1>Perfil</h1>
+            <div class="info">
+
+
+
+
+               <table>
+                    <tr>
+                         <th></th>
+                         <th>NOMBRE</th>
+                         <th>APELLIDO</th>
+                    </tr>
+                    <?php 
+                    if ($result->num_rows > 0) {
+                         // Mostrar datos de cada fila
+                         while ($row = $result->fetch_assoc()) {
+                              echo " 
+                              <tr>
+                              <td><img class='iconUser' src='Image\user.png' alt='Imgen User'></td>
+                                   <td>".$row["NOMBRE"]."</td>
+                                   <td>".$row["APELLIDO"]."</td>
+                              </tr>";
+                         }
+                    }
+                    ?>
+               </table>
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+        <div class="userInvoid">
+            <h1>Clientes y Consultas</h1>
+            <table>
+                <tr>
                     <th>ID Cliente</th>
                     <th>NOMBRE</th>
                     <th>TELEFONO</th>
@@ -52,31 +92,32 @@ $result = $conn->query($sql);
                     <th>TRATAMIENTO</th>
                     <th>FECHA FACTURA</th>
 
-               </tr>
-               <?php
+                </tr>
+                <?php
                if ($result->num_rows > 0) {
                     // Mostrar datos de cada fila
                     while ($row = $result->fetch_assoc()) {
                          echo "<tr>
-                       
-                        <td>" . $row["NOMBRE"] . "</td>
-                        <td>" . $row["APELLIDO"] . "</td>                        
-                        <td>" . $row["EMAIL"] . "</td>
-                        <td>" . $row["CEDULA"] . "</td>                        
-                        <td>" . $row["FECHA_FACTURA"] . "</td>
-                        <td>" . $row["PHONE"] . "</td>
-                        <td>" . $row["TRATAMIENTO"] . "</td>
-                        <td>" . $row["FECHA_FACTURA"] . "</td>
-
-                       
-                    
-                      </tr>";
+                         
+                         <td>" . $row["NOMBRE"] . "</td>
+                         <td>" . $row["APELLIDO"] . "</td>                        
+                         <td>" . $row["EMAIL"] . "</td>
+                         <td>" . $row["CEDULA"] . "</td>                        
+                         <td>" . $row["FECHA_FACTURA"] . "</td>
+                         <td>" . $row["PHONE"] . "</td>
+                         <td>" . $row["TRATAMIENTO"] . "</td>
+                         <td>" . $row["FECHA_FACTURA"] . "</td>
+                         </tr>
+                         </table>";
                     }
                } else {
                     echo "<tr><td colspan='12'>No hay resultados</td></tr>";
                }
                ?>
-     </div>
+        </div>
+        <!--  -->
+
+    </div>
 </body>
 
 </html>
